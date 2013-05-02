43bf326b61ea971c53fd24335cb2209b44409177
Brandon Allan Rupert <brandon.a.rupert@gmail.com>
2013-05-02 19:37:04 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Othertrial::Application.config.secret_token = '26f2b15f16296ee71210daee67edaffacd18a2285e31fe7b6e5abecbcfc05f2aaeae43553ccf938ae09aa8a209a90d0be99b378dea71ba988b672d737a1c4185'
